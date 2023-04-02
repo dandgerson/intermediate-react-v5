a346049b729bf660beaf8d963f51f8d3d8746b43
Dmitry G. Anderson <dandgerson@gmail.com>
2023-04-02 10:43:04 +0300

    layout with tailwindCSS

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -4,11 +4,11 @@ export const Pet = ({ name, animal, breed, location, id, images }) => {
   const hero = "http://pets-images.dev-apis.com/pets/none.jpg";
 
   return (
-    <Link to={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="relative block">
       <div className="image-container">
         <img src={images.length ? images[0] : hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
--- src/Results.jsx
+++ src/Results.jsx
@@ -1,7 +1,7 @@
 import { Pet } from "./Pet";
 
 export const Results = ({ pets }) => (
-  <div className="search">
+  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
     {pets.length ? (
       pets.map((pet) => (
         <Pet
